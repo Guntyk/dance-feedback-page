1085f0737d4a9ee75817771e2ec8760aaf4d4e84
Gunter <mashyn.daniil@gmail.com>
2023-06-15 15:26:48 +0300

    Header Done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="main-title">Звернення до танцювальної спільноти</h1>
+    </header>
+  );
+}
